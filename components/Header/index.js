0233c65539db990a88682f0d72c2ecce9dccaf43
brittanymae01 <brittanymae01>
2019-11-01 08:20:10 -0700

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const newHeader = document.querySelector('.header-container');
+newHeader.appendChild(Header());
+
+function Header() {
+
+    const header = document.createElement('div');
+    const span1 = document.createElement('span');
+    const title = document.createElement('h1');
+    const span2 = document.createElement('span');
+
+    header.classList.add('header');
+    span1.classList.add('date');
+    span2.classList.add('temp');
+
+    span1.textContent = 'SMARCH 28, 2019';
+    title.textContent = 'Lambda Times';
+    span2.textContent = '98°';
+
+    header.appendChild(span1);
+    header.appendChild(title);
+    header.appendChild(span2);
+
+    return header
+}
